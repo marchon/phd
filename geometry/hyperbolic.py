3d65a4f308de37d970e81fed9a85d1a4e04ccc24
marchon <marchon@gmail.com>
2014-11-02 01:52:40 -0400

    added information about the author and request for License Clarification

--- geometry/hyperbolic.py
+++ geometry/hyperbolic.py
@@ -1,3 +1,54 @@
+""" 
+LICENSE TYPE: UNKNOWN 
+
+    I have requested from the original author use under the MIT License
+    
+    his name is: 
+    his website is: http://www.flatown.com/about.html
+    his email address is: 
+    original github repo: https://github.com/jflatow/phd/blob/master/geometry/hyperbolic.py  
+
+    The Coolest thing that he put in his about page:
+               
+             "I'm especially interested in 
+              the relationship between humans and machines. 
+              I like to build things that fit human intuition. 
+              I want to understand why people do what they do, 
+              and how to build systems that are 
+              robust to our flaws.
+             " 
+
+    It is an interesting and "Bell Curvish" statement.
+    When I get a response from him I will update the header to this file. 
+""" 
+_License = 'Unknown'  
+_ProposedLicense = 'MIT' 
+
+"""
+The MIT License
+
+Copyright (c) Jared Flatow 2014   http://www.flatown.com 
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+""" 
+
+
 """
 Some rational universal hyperbolic geometry as per the inimitable Norman J. Wildberger.
 
